d44ac66311618ee46a27c5dc3bbdf06b9ec4e4e6
Marco Wallner <marco.wallner@ait.ac.at>
2024-01-08 16:32:34 +0100

    added openrmf specific stuff

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ set -e
 # Set the default build type
 BUILD_TYPE=RelWithDebInfo
 colcon build \
+        --parallel-workers 8 \
         --merge-install \
         --symlink-install \
         --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
-        -Wall -Wextra -Wpedantic
+        -Wall -Wextra -Wpedantic 
--- setup.sh
+++ setup.sh
@@ -5,3 +5,24 @@ vcs import < src/ros2.repos src
 sudo apt-get update
 rosdep update
 rosdep install --from-paths src --ignore-src -y
+
+# install nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
+nvm install 16
+
+# install pnpm
+curl -fsSL https://get.pnpm.io/install.sh | bash -
+export PNPM_HOME="/home/ros/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+pnpm env use --global 16
+
+# install rmf-web deps
+pushd /workspaces/ws_open_rmf/src/rmf-web
+pnpm install
+popd
